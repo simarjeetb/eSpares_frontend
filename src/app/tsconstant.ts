da6efe12e80352f689f1d828ec33e4907a14ba4f
Simarjeet Kaur <simarjeetb@smartdatainc.net>
2017-07-07 16:02:27 +0530

    Update tsconstant.ts
    tsconstant changes

--- src/app/tsconstant.ts
+++ src/app/tsconstant.ts
@@ -1,5 +1,5 @@
 'use strict';
 
 export const SEP='/';
-export const HOST: string="http://localhost:4076";
-export const CLIENT_ID: string = 'xpZqUQlF5eOzpV6oqFaKfjbQrPh7yb8F'
+export const HOST: string="http://52.39.212.226:4076";
+export const CLIENT_ID: string = '1vgDb7CiwCrglrCguvLmzyIyxfiDd1wZ'
